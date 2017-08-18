80189ab7c4cd05f53732781a114766fb3a8657cc
Lucas <lfbodereau@gmail.com>
2017-08-18 15:42:17 +0200

    first test passed and seccond middle

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,28 @@
 // Soldier
-function Soldier () {}
+function Soldier (health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function() {
+    return this.strength;
+  };
+  this.receiveDamage = function(damage) {
+    this.strength -= 50;
+  };
+
+};
+
+soldier.attack(soldier.strength);
+
 
 // Viking
-function Viking () {}
+function Viking () {
+  Soldier.call(this, health, strength, attack, receiveDamage);
+};
+
+Viking.prototype.constructor = Soldier;
+
+viking.attack();
+
 
 // Saxon
 function Saxon () {}
